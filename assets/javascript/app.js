f3543c78dfa48e2532537b24b1e644e5724d4170
Adrian Divecha <ajdivecha@macbook-pro-2.home>
2017-10-30 23:03:36 -0700

    start

---
+++ assets/javascript/app.js
@@ -0,0 +1,70 @@
+
+var questions = {
+    q1: ["On April 18, 1958, the then Brooklyn Dodgers played their first game as the LA Dodgers. What venue served as the site for their opening day game?"],
+    //Los Angeles Coliseum --> Dodger Stadium, Athletic Park, Angel Stadium
+
+    q2: ["Which former Red Sox player wore the number 99 for the Dodgers in 2009?"],
+    //Manny Ramirez --> Dave Roberts, David Ortiz, Adrian Gonzalez
+
+    q3: ["Who was the manager of the LA Doders for the 2008 season?"],
+    //Joe Torree --> Tommy Lasorda, Don Mattingly, Dave Roberts
+
+    q4: ["Who was the captain of the 1978 Dodgers?"],
+    //Davey Lopes --> Dusty Baker, Steve Garvey, Don Sutton
+
+    q5: ["When right fielder, Shawn Green, broke the Dodgers franchise record, in 2001, for most homers in a season, whose record did he beat?"],
+    //Gary Sheffield --> Willie Keeler, Zack Wheat, Duke Snider
+
+    q6: ["Who wa the first player for the Dodgers franchise to win the NL MVP award?"],
+    //Dolph Camilli --> Jackie Robinson, Sandy Koufax, Clayton Kershaw
+
+    q7: ["Which former Texas Rangers ace pitcher the the Dodgers trade for the season?"],
+    //Yu Darvish --> Cole Hamels, Matt Bush, Keone Kela
+
+    q8: ["What Dodger set a World Series record when he hit two pinch hit homers in the 1959 World Series?"],
+    //Chuck Essegian --> Maury Willis, Frank Howard, Duke Snider
+
+    q9: ["What Dodger of the 20th Century has the highest career strikeout ratio per nine innings pitched with at least two full seasons with the Dodgers?"],
+    //Hideo Nomo --> Clayton Kershaw, Greg Maddux, Bob Gibson
+
+    q10: ["From the first Dodgers' game played in Los Angeles (1958), until the end of the 2008 season (50 years later), who managed the team for the most number of full seasons?"],
+    //Tommy Lasorda --> Joe Torre, Don Mattingly, Dave Roberts
+};
+
+var questionIndex = 0;
+
+var questionsArray = [questions.q1, questions.q2, questions.q3, questions.q4, questions.q5, questions.q6, questions.q7, questions.q8, questions.q9, questions.q10];
+//so questoins above know where to choose
+
+var correct = 0;
+
+var wrong = 0;
+
+var countDownTimer = 0;
+// // nest a timer function inside the variable --- set timer to 15000 miliseconds --- set a reset function --- set a start function --- set a stop function --- if/else statements
+
+
+// //var q1-q10 --- do we make each questoin a variable so we can dispaly a new question on each page? If so, how do we set them up (Q - answers[A-D] - correct answer)
+
+
+// //load the question and answers
+
+
+// //set up event listerners for each of the buttons .on("click")
+
+
+// //correct answer function
+
+
+// //wrong answer function
+
+
+// //update correct/wrong
+
+
+// //display totals to html 
+
+
+
+
+
